a4bafcaca428df3b79a6d4372ce8c6461f893628
shootime2021 <13760404575@163.com>
2025-02-21 16:41:34 +0800

    save qwen models

--- trans/mla/infer_ds_qwen.py
+++ trans/mla/infer_ds_qwen.py
@@ -2,7 +2,7 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
 from qwen2.modeling_qwen2_v1 import Qwen2ForCausalLM
 
-ds_path = "/data/h3030118/data/models--deepseek-ai--DeepSeek-R1-Distill-Qwen-32B/snapshots/3865e12a1eb7cbd641ab3f9dfc28c588c6b0c1e9"
+ds_path = "/data/vayu/train/models/DeepSeek-R1-Distill-Qwen-32B"
 
 # 加载模型和分词器
 tokenizer = AutoTokenizer.from_pretrained(ds_path, trust_remote_code=True)
@@ -24,6 +24,15 @@ for name,module in model.named_modules():
     if 'k_up_proj' in name or "v_up_proj" in name:
         module.weight.data = torch.stack([torch.eye(kv_heads*head_dim).reshape(kv_heads, head_dim, kv_heads*head_dim)]*kv_groups,dim=1).reshape(hidden_size, kv_heads*head_dim).contiguous().to(module.weight.data.device,module.weight.data.dtype)
 
+# 保存修改后的模型和tokenizer
+import os
+save_dir = "saves"
+model_name = "qwen_eye_matrix_kv_proj"  # 反映了对k_up_proj和v_up_proj使用eye matrix的改动
+save_path = os.path.join(save_dir, model_name)
+os.makedirs(save_path, exist_ok=True)
+model.save_pretrained(save_path)
+tokenizer.save_pretrained(save_path)
+print(f"Model and tokenizer saved to {save_path}")
 
 # 输入文本
 prompt = "如何做西红柿炒鸡蛋？"
