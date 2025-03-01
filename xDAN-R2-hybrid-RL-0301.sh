92d7eaf26e56ca02bf1a1e61d3fe9d69eb9a62cb
shootime2021 <13760404575@163.com>
2025-03-01 10:10:41 +0800

    Add xDAN-R2-hybrid-RL-0301.sh script

---
+++ xDAN-R2-hybrid-RL-0301.sh
@@ -0,0 +1,67 @@
+set -x
+DATASET="./examples/data/xDAN-Terrible-level-math-collection_chatml_rl.json"
+
+MODEL_CPK_NAME="xDAN-L2-RL-32B-Instruct"
+PRETRAIN_MODEL="/data/vayu/train/models/xDAN-L2-32b-Reasoning-SFT-Alignment-0216-ckp2364"
+SAVE_PATH="./ckpts"
+mkdir -p "${SAVE_PATH}/${MODEL_CPK_NAME}"
+mkdir -p "${SAVE_PATH}/${MODEL_CPK_NAME}/tensorboard"
+
+# deploy remote reward function at 127.0.0.1:5000
+python -m openrlhf.models.remote_rm.math_verifier --dataset $DATASET --input_key prompt --prompt-template chatml > "${SAVE_PATH}/${MODEL_CPK_NAME}/remote_rm.log" 2>&1 &
+childpid=$!
+
+
+ray job submit --address="http://0.0.0.0:8265" \
+   --runtime-env-json='{"working_dir": "/data/vayu/train/xDAN-RL-Training-GRPO", "env_vars": {"MASTER_ADDR": "10.11.50.36", "MASTER_PORT": "24999"}}' \
+   -- python3 -m openrlhf.cli.train_ppo_ray \
+   --ref_num_nodes 1 \
+   --ref_num_gpus_per_node 4 \
+   --actor_num_nodes 1 \
+   --actor_num_gpus_per_node 4 \
+   --vllm_num_engines 2 \
+   --vllm_tensor_parallel_size 4 \
+   --remote_rm_url http://localhost:5000/get_reward \
+   --vllm_gpu_memory_utilization 0.7 \
+   --advantage_estimator rloo \
+   --pretrain $PRETRAIN_MODEL \
+   --save_path $SAVE_PATH/$MODEL_CPK_NAME \
+   --ckpt_path $SAVE_PATH/$MODEL_CPK_NAME/ckpt \
+   --save_hf_ckpt \
+   --micro_train_batch_size 2 \
+   --train_batch_size 64 \
+   --micro_rollout_batch_size 4 \
+   --rollout_batch_size 1024 \
+   --n_samples_per_prompt 8 \
+   --max_epochs 1 \
+   --prompt_max_len 1024 \
+   --max_samples 100000 \
+   --generate_max_len 1024 \
+   --per_device_train_batch_size 1 \
+   --gradient_accumulation_steps 4 \
+   --zero_stage 3 \
+   --offload_optimizer disk \
+   --offload_param disk \
+   --memory_efficient_linear True \
+   --actor_learning_rate 5e-7 \
+   --critic_learning_rate 9e-6 \
+   --init_kl_coef 1e-4 \
+   --prompt_data $DATASET \
+   --input_key prompt \
+   --apply_chat_template \
+   --normalize_reward \
+   --adam_offload \
+   --gradient_checkpointing \
+   --packing_samples \
+   --vllm_sync_backend gloo \
+   --enforce_eager \
+   --vllm_enable_sleep \
+   --save_steps 200 \
+   --ckpt_path $SAVE_PATH/$MODEL_CPK_NAME/ckpt \
+   --save_hf_ckpt \
+   --use_wandb $SAVE_PATH/$MODEL_CPK_NAME/logs 
+# You could also try
+#   --use_kl_loss \
+#   --use_kl_estimator_k3 \
+
+# also supports --advantage_estimator rloo | reinforce_baseline
